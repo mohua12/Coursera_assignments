d3bdea3e7f4f6414d0034985e397e49a33932fc5
mohua12 <76768623+mohua12@users.noreply.github.com>
2021-01-04 10:53:05 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
   .controller('myController',function($scope){
     $scope.lunchitem="";
 $scope.AddItem=function(){
-  if(angular.isUndefinedOrNullOrEmpty($scope.lunchitem)
+  if(angular.isUndefinedOrNullOrEmpty($scope.lunchitem))
   {
     alert("Please Please enter data first");
     return;
