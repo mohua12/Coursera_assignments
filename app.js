047a6d8addc4b2692f8893977d9d198231a099cd
mohua12 <76768623+mohua12@users.noreply.github.com>
2021-01-04 10:32:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
   angular.module('myapp',[])
   .controller('myController',function($scope){
 $scope.AddItem=function(){
-
+if($scope.lunchitem=="")
+{
+  alert("Please Please enter data first");
+  return;
+}
   var items = $scope.lunchitem.split(',');
   if(items.length<=3)
   {
